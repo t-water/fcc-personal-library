cc02f65e43423ab2c794740bcb85605176ed1dd0
Glitch (noon-porkpie) <none>
2019-10-25 19:58:58 +0000

    🍏💵 Checkpoint ./routes/api.js:6684026/1282

--- routes/api.js
+++ routes/api.js
@@ -43,11 +43,17 @@ module.exports = function (app) {
       Books.findById(req.body._id)
       .then(book => {
         if(book != null){
-          
+          Books.findByIdAndRemove(req.body._id)
+          .then(book => {
+            res.statusCode = 200;
+            res.setHeader('Content-type', 'application/text')
+            res.send('Delete successful')
+          }, err => next(err))
+          .catch(err => next(err))
         }else{
-          err = 
-          res.statusCode = 404;
-          
+          let err = new Error('Book not found') 
+          err.status = 404;
+          return next(err);
         }
       },err => next(err))
       .catch(err => next(err))
@@ -56,15 +62,41 @@ module.exports = function (app) {
 
 
   app.route('/api/books/:id')
-    .get(function (req, res){
+    .get(function (req, res, next){
       var bookid = req.params.id;
+      Books.findById(bookid)
+      .then(book => {
+        if(book != null){
+          res.statusCode = 200;
+          res.setHeader('Content-type', 'application/json');
+          res.json(book);
+        }else{
+          let err = new Error('Book not found');
+          err.status = 404;
+          return next(err)
+        }
+      }, err => next(err))
+      .catch(err => next(err))
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
     
-    .post(function(req, res){
+    .post(function(req, res, next){
       var bookid = req.params.id;
       var comment = req.body.comment;
-      //json res format same as .get
+      Books.findById(req.params.id)
+      .then(book => {
+        if(book != null){
+          book.comments.push(comment);
+          res.status = 200;
+          res.setHeader('application/json')
+          res.json(book)
+        }else{
+          let err = new Error('Book not found');
+          err.status = 404;
+          return next(err);
+        }
+      }, err => next(err))
+      .catch(err => next(err))
     })
     
     .delete(function(req, res){
