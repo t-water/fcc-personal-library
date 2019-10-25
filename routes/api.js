6ff67f6e2f2052e5b4df9b5229a5ea4ec893fe14
Glitch (noon-porkpie) <none>
2019-10-25 19:47:56 +0000

    💷🍶 Checkpoint ./routes/api.js:6684026/1129

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,7 @@
 
 var expect = require('chai').expect;
 const mongoose = require('mongoose');
-const Books = require('./books');
+const Books = require('../books');
 var ObjectId = require('mongodb').ObjectId;
 const MONGOOSE_CONNECTION_STRING = process.env.DB;
 mongoose.connect(MONGOOSE_CONNECTION_STRING, {useNewUrlParser: true, useFindAndModify: false});
@@ -18,23 +18,39 @@ mongoose.connect(MONGOOSE_CONNECTION_STRING, {useNewUrlParser: true, useFindAndM
 module.exports = function (app) {
 
   app.route('/api/books')
-    .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+    .get(function (req, res, next){
+      Books.find({})
+      .then(books => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(books)
+      },err => next(err))
+      .catch(err => next(err))
     })
     
-    .post(function (req, res){
+    .post(function (req, res, next){
       var title = req.body.title;
-      Books.insert(req.body)
+      Books.create(req.body)
       .then(book => {
-        
-      })
-      .catch(err => res.json(err));
-      //response will contain new book object including atleast _id and title
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'application/json');
+        res.json(book);
+      },err => next(err))
+      .catch(err => next(err));
     })
     
-    .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
+    .delete(function(req, res, next){
+      Books.findById(req.body._id)
+      .then(book => {
+        if(book != null){
+          
+        }else{
+          err = 
+          res.statusCode = 404;
+          
+        }
+      },err => next(err))
+      .catch(err => next(err))
     });
 
 
